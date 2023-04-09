e12939b58d267865b93141b86063bfdb73c7a45c
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2023-04-08 22:15:58 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-// Write your code here
+// Function multiply will emulate an Equation that will always return a product of 62;
+const num1 = 62;
+const num2 = 1;
+const num3 = 16;
+const num4 = 4;
+
+
+const multiply = num1*num2;
+
+
+const random = (Math.ceil(Math.random()+1));
+
+
+const mod = num3/num4;
+
+
+const max = Math.max(1, 5, 8, 20);
+
+
+
